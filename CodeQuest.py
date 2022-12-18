8f3c497301d6bac4a961cc869d83c73f230c3916
Ayush Morbar <108860844+ayushmorbar@users.noreply.github.com>
2022-12-18 11:37:48 +0530

    Finished the quest.

--- CodeQuest.py
+++ CodeQuest.py
@@ -20,3 +20,74 @@ MyAnswer(list)
 
 
 
+# Welcome to the Lumos Coding Quest
+
+# you have been given a linked list with duplicate values
+
+# upon running this code, you will see a string that has been formed by collecting the data in the nodes of the linked list
+
+# you need to complete the "MyAnswer" function and ensure that it removes the duplicate nodes from the linkedlist
+
+# when you collate the string that this new linked list contains, you will get the link to the whitelist form
+
+# Note:- "list" represents the said linked list
+
+from helper import *
+
+def MyAnswer(self):
+
+   Node ptr1 = null, ptr2 = null, dup = null;
+
+        ptr1 = head;
+
+ 
+
+        /* Pick elements one by one */
+
+        while (ptr1 != null && ptr1.next != null) {
+
+            ptr2 = ptr1;
+
+ 
+
+            /* Compare the picked element with rest
+
+                of the elements */
+
+            while (ptr2.next != null) {
+
+ 
+
+                /* If duplicate then delete it */
+
+                if (ptr1.data == ptr2.next.data) {
+
+ 
+
+                    /* sequence of steps is important here
+
+                     */
+
+                    ptr2.next = ptr2.next.next;
+
+                    System.gc();
+
+                }
+
+                else /* This is tricky */ {
+
+                    ptr2 = ptr2.next;
+
+                }
+
+            }
+
+            ptr1 = ptr1.next;
+
+        }
+
+   answer = ""
+
+   print(answer)
+
+MyAnswer(list)
